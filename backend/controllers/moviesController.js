ea67065c8b8e49339dfcdef7cff22af437a2e438
dpark001 <dparkzero1@gmail.com>
2024-04-13 18:56:36 -0700

    waiting for work to begin on databases

--- backend/controllers/moviesController.js
+++ backend/controllers/moviesController.js
@@ -18,12 +18,16 @@ const options = {
 
 const MoviesController = {
   getByFilters: async (req, res, next) => {
-    const { country, genres } = req.params;
-    const services = 'netflix,prime.subscription,prime.rent,prime.buy,apple.rent,apple.buy,hbo,hulu.addon.hbo,prime.addon.hbomaxus,hulu.subscription,hulu.addon.hbo,apple.addon,peacock.free';
     // const genres = '10749,35';
-    const nextCursor = '113478:조선명탐정: 흡혈괴마의 비밀';
+    // const nextCursor = '113478:조선명탐정: 흡혈괴마의 비밀';
+    
+    const { genres, nextCursor } = req.params;
+    const services = 'netflix,prime.subscription,prime.rent,prime.buy,apple.rent,apple.buy,hbo,hulu.addon.hbo,prime.addon.hbomaxus,hulu.subscription,hulu.addon.hbo,apple.addon,peacock.free';
+    // const cursor = `&cursor=${nextCursor}`;
+    let cursor;
+    if (nextCursor) cursor = `cursor=${nextCursor}`;
 
-    const url = `https://streaming-availability.p.rapidapi.com/search/filters?services=${services}&country=us&output_language=en&order_by=original_title&genres=${genres}&genres_relation=or&desc=true&show_type=movie&cursor=${nextCursor}`;
+    const url = `https://streaming-availability.p.rapidapi.com/search/filters?services=${services}&country=us&output_language=en&order_by=original_title&genres=${genres}&genres_relation=or&desc=true&show_type=movie&${cursor}`;
 
     try {
       const response = await fetch(url, options);
