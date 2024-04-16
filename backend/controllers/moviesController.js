68b03888a865a1da025157e6609b9c2948b0ba30
dpark001 <dparkzero1@gmail.com>
2024-04-15 19:38:01 -0700

    refactor moviesController for new API

--- backend/controllers/moviesController.js
+++ backend/controllers/moviesController.js
@@ -1,14 +1,14 @@
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'f267b612b6msh504ab02ca095331p142270jsn257fad83de1e',
-    'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjk4MDlkNmE4OTE3ZTY4YjJlZDYxZjdhYWYwODFjMyIsInN1YiI6IjY2MWJlODY1ZGJhZDUwMDE3YjQzOTViMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JTk0Fw08cJtkM9mQC_O7-jAj67h9O5E4p4_JAZRsRGg'
   }
 };
 
 const moviesController = {
-  getByFilters: async (req, res, next) => {
-    const { genres, nextCursor } = req.params;
+  getMovieList: async (req, res, next) => {
+    const { genreId } = req.params;
 
     const services = 'netflix,prime.subscription,prime.rent,prime.buy,apple.rent,apple.buy,hbo,hulu.addon.hbo,prime.addon.hbomaxus,hulu.subscription,hulu.addon.hbo,apple.addon,peacock.free';
     const country = 'us';
@@ -17,19 +17,34 @@ const moviesController = {
     const genres_relation = 'or';
     const desc = 'true';
     const show_type = 'movie';
-    let cursor;
-    if (nextCursor) cursor = `cursor=${nextCursor}`;
+    
 
-    const url = `https://streaming-availability.p.rapidapi.com/search/filters?services=${services}&country=${country}&output_language=${output_language}&order_by=${order_by}&genres=${genres}&genres_relation=${genres_relation}&desc=${desc}&show_type=${show_type}&${cursor}`;
+    const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&watch_region=us&with_genres=${id}`;
 
     try {
       const response = await fetch(url, options);
       const result = await response.json();
       // console.log(result);
-      res.locals.movies = result;
+      res.locals.movieList = result;
       return next();
-    } catch (error) {
-      return next({ log: 'Error in MoviesController.getByFilters: ', error });
+    } catch (err) {
+      return next({ log: 'Error in moviesController.getMovieList: ', err });
+    }
+  },
+
+  getMovieDetails: async (req, res, next) => {
+    const { id } = req.params.id;
+    res.locals.movieId = id;
+
+    const deetsUrl = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  
+    try {
+      const response = await fetch(deetsUrl, options);
+      const result = await response.json();
+      res.locals.movieDetails = result;
+      return next();
+    } catch (err) {
+      return next({ log: 'Error in moviesController.getMovieDetails: ', err });
     }
   }
 }
