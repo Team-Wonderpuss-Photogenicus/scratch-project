68b24db9ec68b770b2f052f83a0499107dd780a8
Najm Safar <najmsafarjalani@gmail.com>
2024-04-17 23:52:51 -0400

    connecting backend with frontend displaying movie details

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import Navbar from "./Navbar.jsx";
-import MovieContainer from "../containers/MovieContainer.jsx";
-import Question from "./Question.jsx";
-import MovieRec from "./MovieRec.jsx";
-import styles from "../stylesheet/styles.scss";
+import React, { useState } from 'react';
+import Navbar from './Navbar.jsx';
+import MovieContainer from '../containers/MovieContainer.jsx';
+import Question from './Question.jsx';
+import MovieRec from './MovieRec.jsx';
+import styles from '../stylesheet/styles.scss';
 
 function App() {
-  const [movies, setMovieData] = useState("");
+  const [movies, setMovieData] = useState('');
   const handleMovieData = (data) => {
     setMovieData(data);
   };
   return (
-    <div className="appContainer">
+    <div className='appContainer'>
       <Navbar />
       <MovieRec recommend={handleMovieData} />
-      <Question />
+      <Question recommend={handleMovieData} />
       <MovieContainer movies={movies} />
     </div>
   );
