64a97761e8d73276253fa9ec7a7baf11e70a8b1c
Charlize <charliezhou@outlook.com>
2024-04-18 19:21:59 -0700

    add genre mapping called genres

--- backend/controllers/moviesController.js
+++ backend/controllers/moviesController.js
@@ -1,12 +1,12 @@
-const fsCallback = require('fs');
-const path = require('path');
+const fsCallback = require("fs");
+const path = require("path");
 
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'f267b612b6msh504ab02ca095331p142270jsn257fad83de1e',
+    accept: "f267b612b6msh504ab02ca095331p142270jsn257fad83de1e",
     Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDJkMTBjYmQ5NDRhMjM4ZGEyODY3NmYyNTUwMTAzMSIsInN1YiI6IjY2MWRjOGI4N2FlY2M2MDE2MzZhYzA2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oZo5hhwTMoFBdXLbXC6U0i_M_cYijQzP5oMXg09qc_Y',
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDJkMTBjYmQ5NDRhMjM4ZGEyODY3NmYyNTUwMTAzMSIsInN1YiI6IjY2MWRjOGI4N2FlY2M2MDE2MzZhYzA2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oZo5hhwTMoFBdXLbXC6U0i_M_cYijQzP5oMXg09qc_Y",
   },
 };
 
@@ -20,10 +20,10 @@ const moviesController = {
     const obj = emoList.find((emo) => emo.emotion === emotion);
 
     let with_genres = obj[matchOrEscape];
-    with_genres = with_genres.join('|');
+    with_genres = with_genres.join("|");
 
-    const sort_by = 'popularity.desc';
-    const watch_region = 'us';
+    const sort_by = "popularity.desc";
+    const watch_region = "us";
 
     const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=${sort_by}&watch_region=${watch_region}&with_genres=${with_genres}`;
 
@@ -31,10 +31,24 @@ const moviesController = {
       const response = await fetch(url, options);
       const result = await response.json();
       // console.log(result);
-      res.locals.list = result;
+      //add genre mapping function
+      const genreMappingPath = path.join(
+        __dirname,
+        "..",
+        "data",
+        "genreMapping.json"
+      );
+      const genreMappingData = fsCallback.readFileSync(
+        genreMappingPath,
+        "utf8"
+      );
+      const genreMapping = JSON.parse(genreMappingData);
+      const test = getGenreId(result, genreMapping);
+
+      res.locals.list = test;
       return next();
     } catch (err) {
-      return next({ log: 'Error in moviesController.getMovieList: ', err });
+      return next({ log: "Error in moviesController.getMovieList: ", err });
     }
   },
 
@@ -50,7 +64,7 @@ const moviesController = {
       // console.log(res.locals.details);
       return next();
     } catch (err) {
-      return next({ log: 'Error in moviesController.getMovieDetails: ', err });
+      return next({ log: "Error in moviesController.getMovieDetails: ", err });
     }
   },
 
@@ -66,11 +80,26 @@ const moviesController = {
       return next();
     } catch (err) {
       return next({
-        log: 'Error in moviesController.getWatchProviders: ',
+        log: "Error in moviesController.getWatchProviders: ",
         err,
       });
     }
   },
 };
+
+function getGenreId(result, genreMapping) {
+  const mappedResult = result.results.map((movie) => {
+    const genres = movie.genre_ids.map((genreId) => {
+      const genre = genreMapping.genres.find((g) => g.id === genreId);
+
+      //console.log(`genre ${genre.name}`);
+      return genre.name;
+    });
+    //console.log(`genres ${genres}`);
+
+    return { ...movie, genres: genres.join(",") };
+  });
+  return { ...result, results: mappedResult };
+}
 
 module.exports = moviesController;
